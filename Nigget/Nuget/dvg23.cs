f1fb4495c0ac055be91f59c87f556c00ca8c2aca
DimaX456 <106971904+DimaX456@users.noreply.github.com>
2022-11-18 18:37:02 +0300

    Add files via upload

---
+++ Nigget/Nuget/dvg23.cs
@@ -0,0 +1,26 @@
+﻿namespace Nigget
+{
+    public class dvg23<T> where T : class
+    {
+        private List<T> value = new List<T>();
+
+        public dvg23() { }
+        public List<T> abiturientst()
+        {
+            return value;
+        }
+        public void Addbit(T abiturient)
+        {
+            value.Add(abiturient);
+        }
+        public void Deletebit (T abiturient)
+        {
+            value.Remove(abiturient);
+        }
+        public void Changebit (T abiturient, T Id)
+        {
+            var index = value.IndexOf (Id);
+            value[index] = abiturient;
+        }
+    }
+}
